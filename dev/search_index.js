631c8f9e54db4072c1fedd7024c59d9027d524fc
Documenter.jl <documenter@juliadocs.github.io>
2024-04-06 07:17:05 +0000

    build based on 427965b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CatppuccinMakieThemes","category":"page"},{"location":"#CatppuccinMakieThemes","page":"Home","title":"CatppuccinMakieThemes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CatppuccinMakieThemes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CatppuccinMakieThemes]","category":"page"}]
+}
