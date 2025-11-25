47a44a0c045ead17b8c86778c5cb252106c7fadb
Documenter.jl <documenter@juliadocs.github.io>
2025-11-25 08:53:39 +0000

    build based on 786e102

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#CatppuccinMakieThemes","page":"Home","title":"CatppuccinMakieThemes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CatppuccinMakieThemes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#CatppuccinMakieThemes","page":"Home","title":"CatppuccinMakieThemes","text":"Documentation for CatppuccinMakieThemes.\n\n","category":"section"}]
 }
